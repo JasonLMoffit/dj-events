9ea4ecfc63b967e04cc8e120c07f8a9855d8a213
JasonLMoffit <j.moffit@hotmail.com>
2022-01-12 15:11:27 -0600

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const Root = ({ Component, pageProps }) => <Component {...pageProps} />;
 
-export default MyApp
+export default Root;
---
+++ pages/event/add.js
@@ -0,0 +1,7 @@
+export default function AddEventPage() {
+  return (
+    <div>
+      <h2>Add Event</h2>
+    </div>
+  );
+}
